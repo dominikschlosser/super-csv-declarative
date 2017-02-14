a9fd9ebf2455f87a9827ac03634046de2d4178d1
thomas-mc-work <thomas.mc.work@gmail.com>
2017-02-14 10:21:17 +0100

    add an option to define the locale for the BigDecimal Parser annotation

--- super-csv-declarative/src/main/java/com/github/dmn1k/supercsv/io/declarative/annotation/ParseBigDecimal.java
+++ super-csv-declarative/src/main/java/com/github/dmn1k/supercsv/io/declarative/annotation/ParseBigDecimal.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2007 Kasper B. Graversen
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,15 +15,14 @@
  */
 package com.github.dmn1k.supercsv.io.declarative.annotation;
 
+import com.github.dmn1k.supercsv.io.declarative.CellProcessorAnnotationDescriptor;
+import com.github.dmn1k.supercsv.io.declarative.ProcessorOrder;
+import com.github.dmn1k.supercsv.io.declarative.StandardCsvContexts;
+import com.github.dmn1k.supercsv.io.declarative.provider.ParseBigDecimalCellProcessorProvider;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-
-import com.github.dmn1k.supercsv.io.declarative.ProcessorOrder;
-import com.github.dmn1k.supercsv.io.declarative.StandardCsvContexts;
-import com.github.dmn1k.supercsv.io.declarative.provider.ParseBigDecimalCellProcessorProvider;
-import com.github.dmn1k.supercsv.io.declarative.CellProcessorAnnotationDescriptor;
 
 /**
  * Annotation for the {@link org.supercsv.cellprocessor.ParseBigDecimal}-cell processor
@@ -37,4 +36,8 @@ import com.github.dmn1k.supercsv.io.declarative.CellProcessorAnnotationDescripto
 public @interface ParseBigDecimal {
 
     int order() default ProcessorOrder.UNDEFINED;
+    /**
+     * Locale definition as found in {@link java.util.Locale#forLanguageTag}.
+     */
+    String locale();
 }
